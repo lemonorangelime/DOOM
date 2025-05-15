592b870db81e27324f29e080b31b4e60a9ac3ef2
kitty14956590 <borbtrash@gmail.com>
2025-05-15 01:37:46 -0400

    start porting

---
+++ lemonosdoom-1.10/i_video.c
@@ -0,0 +1,420 @@
+// Emacs style mode select   -*- C++ -*- 
+//-----------------------------------------------------------------------------
+//
+// $Id:$
+//
+// Copyright (C) 1993-1996 by id Software, Inc.
+//
+// This source is available for distribution and/or modification
+// only under the terms of the DOOM Source Code License as
+// published by id Software. All rights reserved.
+//
+// The source is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// FITNESS FOR A PARTICULAR PURPOSE. See the DOOM Source Code License
+// for more details.
+//
+// $Log:$
+//
+// DESCRIPTION:
+//	DOOM graphics stuff for X11, UNIX.
+//
+//-----------------------------------------------------------------------------
+
+static const char
+rcsid[] = "$Id: i_x.c,v 1.6 1997/02/03 22:45:10 b1 Exp $";
+
+#include <stdlib.h>
+#include <unistd.h>
+
+//#include <X11/Xlib.h>
+//#include <X11/Xutil.h>
+//#include <X11/keysym.h>
+#include <runtime.h>
+#include <graphics.h>
+
+#include <stdarg.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+//#include <errno.h>
+//#include <signal.h>
+
+#include "doomstat.h"
+#include "i_system.h"
+#include "v_video.h"
+#include "m_argv.h"
+#include "d_main.h"
+
+#include "doomdef.h"
+
+#define POINTER_WARP_COUNTDOWN	1
+
+typedef struct {
+	void * data;
+} fake_image_t;
+
+window_t * mainWindow;
+
+//Display*	X_display=0;
+//Window		X_mainWindow;
+//Colormap	X_cmap;
+//Visual*		X_visual;
+//GC		X_gc;
+//XEvent		X_event;
+//int		X_screen;
+//XVisualInfo	X_visualinfo;
+fake_image_t *	image;
+int		X_width;
+int		X_height;
+
+// Fake mouse handling.
+// This cannot work properly w/o DGA.
+// Needs an invisible mouse cursor at least.
+boolean		grabMouse;
+int		doPointerWarp = POINTER_WARP_COUNTDOWN;
+
+// Blocky mode,
+// replace each 320x200 pixel with multiply*multiply pixels.
+// According to Dave Taylor, it still is a bonehead thing
+// to use ....
+static int	multiply=1;
+
+
+//
+//  Translates the key currently in X_event
+//
+
+int xlatekey(void)
+{
+
+    int rc; /*
+
+    switch(rc = XKeycodeToKeysym(X_display, X_event.xkey.keycode, 0))
+    {
+      case XK_Left:	rc = KEY_LEFTARROW;	break;
+      case XK_Right:	rc = KEY_RIGHTARROW;	break;
+      case XK_Down:	rc = KEY_DOWNARROW;	break;
+      case XK_Up:	rc = KEY_UPARROW;	break;
+      case XK_Escape:	rc = KEY_ESCAPE;	break;
+      case XK_Return:	rc = KEY_ENTER;		break;
+      case XK_Tab:	rc = KEY_TAB;		break;
+      case XK_F1:	rc = KEY_F1;		break;
+      case XK_F2:	rc = KEY_F2;		break;
+      case XK_F3:	rc = KEY_F3;		break;
+      case XK_F4:	rc = KEY_F4;		break;
+      case XK_F5:	rc = KEY_F5;		break;
+      case XK_F6:	rc = KEY_F6;		break;
+      case XK_F7:	rc = KEY_F7;		break;
+      case XK_F8:	rc = KEY_F8;		break;
+      case XK_F9:	rc = KEY_F9;		break;
+      case XK_F10:	rc = KEY_F10;		break;
+      case XK_F11:	rc = KEY_F11;		break;
+      case XK_F12:	rc = KEY_F12;		break;
+	
+      case XK_BackSpace:
+      case XK_Delete:	rc = KEY_BACKSPACE;	break;
+
+      case XK_Pause:	rc = KEY_PAUSE;		break;
+
+      case XK_KP_Equal:
+      case XK_equal:	rc = KEY_EQUALS;	break;
+
+      case XK_KP_Subtract:
+      case XK_minus:	rc = KEY_MINUS;		break;
+
+      case XK_Shift_L:
+      case XK_Shift_R:
+	rc = KEY_RSHIFT;
+	break;
+	
+      case XK_Control_L:
+      case XK_Control_R:
+	rc = KEY_RCTRL;
+	break;
+	
+      case XK_Alt_L:
+      case XK_Meta_L:
+      case XK_Alt_R:
+      case XK_Meta_R:
+	rc = KEY_RALT;
+	break;
+	
+      default:
+	if (rc >= XK_space && rc <= XK_asciitilde)
+	    rc = rc - XK_space + ' ';
+	if (rc >= 'A' && rc <= 'Z')
+	    rc = rc - 'A' + 'a';
+	break;
+    } */
+
+    return rc;
+
+}
+
+void I_ShutdownGraphics(void)
+{
+  // Paranoia.
+  if (image){
+  	image->data = NULL;
+  }
+}
+
+
+
+//
+// I_StartFrame
+//
+void I_StartFrame (void)
+{
+    // er?
+
+}
+
+static int	lastmousex = 0;
+static int	lastmousey = 0;
+boolean		mousemoved = false;
+
+void I_GetEvent(void)
+{
+
+    doom_event_t event;
+/*
+    // put event-grabbing stuff in here
+    XNextEvent(X_display, &X_event);
+    switch (X_event.type)
+    {
+      case KeyPress:
+	event.type = ev_keydown;
+	event.data1 = xlatekey();
+	D_PostEvent(&event);
+	// fprintf(stderr, "k");
+	break;
+      case KeyRelease:
+	event.type = ev_keyup;
+	event.data1 = xlatekey();
+	D_PostEvent(&event);
+	// fprintf(stderr, "ku");
+	break;
+      case ButtonPress:
+	event.type = ev_mouse;
+	event.data1 =
+	    (X_event.xbutton.state & Button1Mask)
+	    | (X_event.xbutton.state & Button2Mask ? 2 : 0)
+	    | (X_event.xbutton.state & Button3Mask ? 4 : 0)
+	    | (X_event.xbutton.button == Button1)
+	    | (X_event.xbutton.button == Button2 ? 2 : 0)
+	    | (X_event.xbutton.button == Button3 ? 4 : 0);
+	event.data2 = event.data3 = 0;
+	D_PostEvent(&event);
+	// fprintf(stderr, "b");
+	break;
+      case ButtonRelease:
+	event.type = ev_mouse;
+	event.data1 =
+	    (X_event.xbutton.state & Button1Mask)
+	    | (X_event.xbutton.state & Button2Mask ? 2 : 0)
+	    | (X_event.xbutton.state & Button3Mask ? 4 : 0);
+	// suggest parentheses around arithmetic in operand of |
+	event.data1 =
+	    event.data1
+	    ^ (X_event.xbutton.button == Button1 ? 1 : 0)
+	    ^ (X_event.xbutton.button == Button2 ? 2 : 0)
+	    ^ (X_event.xbutton.button == Button3 ? 4 : 0);
+	event.data2 = event.data3 = 0;
+	D_PostEvent(&event);
+	// fprintf(stderr, "bu");
+	break;
+      case MotionNotify:
+	event.type = ev_mouse;
+	event.data1 =
+	    (X_event.xmotion.state & Button1Mask)
+	    | (X_event.xmotion.state & Button2Mask ? 2 : 0)
+	    | (X_event.xmotion.state & Button3Mask ? 4 : 0);
+	event.data2 = (X_event.xmotion.x - lastmousex) << 2;
+	event.data3 = (lastmousey - X_event.xmotion.y) << 2;
+
+	if (event.data2 || event.data3)
+	{
+	    lastmousex = X_event.xmotion.x;
+	    lastmousey = X_event.xmotion.y;
+	    if (X_event.xmotion.x != X_width/2 &&
+		X_event.xmotion.y != X_height/2)
+	    {
+		D_PostEvent(&event);
+		// fprintf(stderr, "m");
+		mousemoved = false;
+	    } else
+	    {
+		mousemoved = true;
+	    }
+	}
+	break;
+	
+      case Expose:
+      case ConfigureNotify:
+	break;
+	
+      default:
+	break;
+    } */
+
+}
+
+//
+// I_StartTic
+//
+void I_StartTic (void)
+{
+
+    if (!mainWindow)
+	return;
+
+    //while (XPending(X_display))
+//	I_GetEvent();
+
+    // Warp the pointer back to the middle of the window
+    //  or it will wander off - that is, the game will
+    //  loose input focus within X11.
+    if (grabMouse)
+    {
+	if (!--doPointerWarp)
+	{ /*
+	    XWarpPointer( X_display,
+			  None,
+			  X_mainWindow,
+			  0, 0,
+			  0, 0,
+			  X_width/2, X_height/2);*/
+
+	    doPointerWarp = POINTER_WARP_COUNTDOWN;
+	}
+    }
+
+    mousemoved = false;
+
+}
+
+
+//
+// I_UpdateNoBlit
+//
+void I_UpdateNoBlit (void)
+{
+    // what is this?
+}
+
+//
+// I_FinishUpdate
+//
+void I_FinishUpdate (void)
+{
+
+    static int	lasttic;
+    int		tics;
+    int		i;
+    // UNUSED static unsigned char *bigscreen=0;
+
+    // draws little dots on the bottom of the screen
+    if (devparm)
+    {
+
+	i = I_GetTime();
+	tics = i - lasttic;
+	lasttic = i;
+	if (tics > 20) tics = 20;
+
+	for (i=0 ; i<tics*2 ; i+=2)
+	    screens[0][ (SCREENHEIGHT-1)*SCREENWIDTH + i] = 0xff;
+	for ( ; i<20*2 ; i+=2)
+	    screens[0][ (SCREENHEIGHT-1)*SCREENWIDTH + i] = 0x0;
+    
+    }
+
+    {
+
+	// draw the image
+	/*XPutImage(	X_display,
+			X_mainWindow,
+			X_gc,
+			image,
+			0, 0,
+			0, 0,
+			X_width, X_height );*/
+
+	// sync up with server
+	//XSync(X_display, False);
+
+    }
+
+}
+
+
+//
+// I_ReadScreen
+//
+void I_ReadScreen (byte* scr)
+{
+    memcpy (scr, screens[0], SCREENWIDTH*SCREENHEIGHT);
+}
+
+
+//
+// I_SetPalette
+//
+void I_SetPalette (byte* palette)
+{
+}
+
+
+void I_InitGraphics(void)
+{
+
+/*    char*		d;
+    int			n;
+    int			pnum;
+    int			x=0;
+    int			y=0;
+    
+    // warning: char format, different type arg
+    char		xsign=' ';
+    char		ysign=' ';
+    
+    int			oktodraw;
+    unsigned long	attribmask;
+    XSetWindowAttributes attribs;
+    XGCValues		xgcvalues;
+    int			valuemask;*/
+    static int		firsttime=1;
+
+    if (!firsttime)
+	return;
+    firsttime = 0;
+
+    //signal(SIGINT, (void (*)(int)) I_Quit);
+
+    X_width = SCREENWIDTH * multiply;
+    X_height = SCREENHEIGHT * multiply;
+
+    mainWindow = NULL;
+    mainWindow = create_window(u"DOOM", u"doom", X_width, X_height);
+    printf("%r\n", mainWindow);
+    if (!mainWindow) {
+        if (!__is_lemonos) {
+		I_Error ("Can't run on non LemonOS-like kernel");
+	} else {
+		I_Error ("Could not open window");
+	}
+    }
+
+    if (multiply == 1)
+	screens[0] = (unsigned char *) (image->data);
+    else
+	screens[0] = (unsigned char *) malloc (SCREENWIDTH * SCREENHEIGHT);
+
+}
+
+
+int	inited;
